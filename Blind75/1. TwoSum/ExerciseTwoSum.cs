42be72bbe648d8e2a861ece2098279e14db73762
Daniel Mendoza <daniel.mendoza@flyrlabs.com>
2024-12-13 23:37:00 +0100

    initial commit

---
+++ Blind75/1. TwoSum/ExerciseTwoSum.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Blind75._1._TwoSum;
+
+public static class ExerciseTwoSum
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        var firstNum = nums[0];
+
+        for (int i = 1; i < nums.Length - 1; i++)
+        {
+
+        }
+
+        return [];
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using LeetCode.Blind75._1._TwoSum;
+
+var nums = new int[] { 2, 7, 11, 15 };
+var target = 9;
+
+var result = ExerciseTwoSum.TwoSum(nums, target);
+
+Console.WriteLine(result);
